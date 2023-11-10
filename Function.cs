d030a648641c4103f0013f1bd6a5ba4a5dcb283e
Sumeet <73847665+sumeetjena01@users.noreply.github.com>
2023-11-10 13:15:04 -0500

    Update Function.cs

--- Function.cs
+++ Function.cs
@@ -2,6 +2,7 @@ using System.Text;
 using Amazon.Lambda.Core;
 using Newtonsoft.Json;
 
+// Sumeet Jena
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
